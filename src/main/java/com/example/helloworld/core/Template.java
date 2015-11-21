e3cf23e7e2a1786f273bcd2a18a8270322fe92d0
hughross <hugh.ross@hmhco.com>
2015-11-21 23:26:12 +0000

    asda
    asda

---
+++ src/main/java/com/example/helloworld/core/Template.java
@@ -0,0 +1,20 @@
+package com.example.helloworld.core;
+
+import com.google.common.base.Optional;
+
+import static java.lang.String.format;
+
+public class Template {
+    private final String content;
+    private final String defaultName;
+
+    public Template(String content, String defaultName) {
+        this.content = content;
+        this.defaultName = defaultName;
+System.out.println(“Done”);
+    }
+    
+    public String render(Optional<String> name) {
+        return format(content, name.or(defaultName));
+    }
+}
